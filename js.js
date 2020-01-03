db70e6e6321e3c2b0312c59b6f6f16e82c03be48
YuChen <46869238+0525033@users.noreply.github.com>
2020-01-04 03:05:26 +0800

    01040305

--- js.js
+++ js.js
@@ -1,30 +1,37 @@
 var x = 0;
 var i = 0;
 var content = document.getElementById("title");
-var NUM = ["<h1>Your Most Reliable Display Partner</h1><h3>We provide transparent demand and supply, real-time manufacturing information, and on-time delivery to serve our valuable customers.</h3>","<h1>Broaden Your Horizons</h1><h3>INNOLUX,the most valuable and competitive all-round solution provider for display technology.</h3>","<h1>Corporate Social Responsibility</h1><h3>INNOLUX, responsibly produce eco-friendly green products for a more sustainable enviroment.</h3>"];
+var NUM = [,,];
 var style = document.getElementById("style")
 
+if(window.screen.width>999){
+    style.href="/Sass/css/style.css"
+    $('#Container').load("index.txt #Container");
+    console.log(1)
+};
+console.log(2)
 // 簡單內容輪播
 function change_content(){
-    if(i==3){i=0};
-    content.innerHTML=NUM[i];
-    i+=1;
+    if(i==0){
+        $("#title").html("<h1>Your Most Reliable Display Partner</h1><h3>We provide transparent demand and supply, real-time manufacturing information, and on-time delivery to serve our valuable customers.</h3>")
+        i+=1
+    }else if(i==1){
+        $("#title").html("<h1>Broaden Your Horizons</h1><h3>INNOLUX,the most valuable and competitive all-round solution provider for display technology.</h3>")
+        i+=1
+    }else{
+        $("#title").html("<h1>Corporate Social Responsibility</h1><h3>INNOLUX, responsibly produce eco-friendly green products for a more sustainable enviroment.</h3>")
+        i=0
+    };
 }
 setInterval(change_content,3000);
 // 簡單內容輪播
-
 document.getElementsByClassName("partner").onmouseover=function(){this.style.color="#000"};
 $("#menu_page").hide();
 $("#back_btn").hide();
 $(function(){
     $("#menu").click(function(){
         $("#menu_page").toggle("fast","swing");
+        $(".back_btn").toggle("fast","swing");
+        // $("#menu").css("background-image","url(/Image/back-button.png)");
     });
 });
-if(window.screen.width>999){
-    console.log(style)
-    style.href="/Sass/css/style.css"
-    console.log(style)
-    $('#Container').load("index.txt #Container");
-    // html_con.innerHTML="1";
-};
